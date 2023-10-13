0ab43a452d99eac32b189f8c200d2ea4d02a2b91
Billy-Not-Really <102433682+Billy-Not-Really@users.noreply.github.com>
2023-10-13 15:00:05 +0300

    Update LocalAD get Disabled user groups.ps1

--- LocalAD get Disabled user groups.ps1
+++ LocalAD get Disabled user groups.ps1
@@ -1,12 +1,14 @@
 $disabledUsers = Get-ADUser -Filter {Enabled -eq $False}
 
 ForEach ($disabledUser in $disabledUsers) {
-    $groupstoRemove = Get-ADPrincipalGroupMembership -Identity $disabledUser
-    Write-Host "$($disabledUser.SamAccountName) remove from groups:"
-    foreach ($group in $groupstoRemove) {
-        if ($group.SamAccountName -ne "Domain Users") {
-            Write-Host $group.SamAccountName
-            # Add the removal logic here, e.g., Remove-ADGroupMember
+    [array]$groupstoRemove = Get-ADPrincipalGroupMembership -Identity $disabledUser
+    if (!($groupstoRemove.Count -eq 1 -and $groupstoRemove[0].SamAccountName -eq "Domain Users")) {
+        Write-Host "$($disabledUser.SamAccountName) remove from groups:"
+        foreach ($group in $groupstoRemove) {
+            if ($group.SamAccountName -ne "Domain Users") {
+                Write-Host "`t $( $group.SamAccountName)"
+                # Add the removal logic here, e.g., Remove-ADGroupMember
+            }
         }
     }
 }
